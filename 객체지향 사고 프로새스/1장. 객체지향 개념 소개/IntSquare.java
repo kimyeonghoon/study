30a7053770bdad2242e1d3f1b646bf0928dda614
me@yeonghoon.kim <me@yeonghoon.kim>
2020-08-15 14:59:22 +0900

    인터페이스 구현부

---
+++ 객체지향 사고 프로새스/1장. 객체지향 개념 소개/IntSquare.java
@@ -0,0 +1,12 @@
+
+public class IntSquare {
+	
+	public int getSquare(int value) {
+		return calculateSquare(value);
+	}
+
+	private int calculateSquare(int value) {
+		return value * value;
+	}
+	
+}
---
+++ 객체지향 사고 프로새스/1장. 객체지향 개념 소개/Payroll.java
@@ -0,0 +1,11 @@
+
+public class Payroll {
+	
+	String name;
+
+	Person p = new Person();
+	
+	p.setName("Joe");
+
+	name = p.getName();
+}
