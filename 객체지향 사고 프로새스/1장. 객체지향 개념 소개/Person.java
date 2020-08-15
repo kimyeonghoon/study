39527d0ecca92478155b873c20256e1a6840afc5
me@yeonghoon.kim <me@yeonghoon.kim>
2020-08-15 14:38:54 +0900

    클래스 정의

---
+++ 객체지향 사고 프로새스/1장. 객체지향 개념 소개/Person.java
@@ -0,0 +1,38 @@
+
+public class Person {
+
+	private String name;
+	private String address;
+	
+	// 이클립스 getter and setter 기능을 이용하여 자동으로 만든 코드
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+/*	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return this.address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	} */
+	
+	
+}
