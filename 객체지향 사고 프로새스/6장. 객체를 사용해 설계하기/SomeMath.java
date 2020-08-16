d493428e4f0d40218ef214c07cce9e912911c430
me@yeonghoon.kim <me@yeonghoon.kim>
2020-08-16 15:51:45 +0900

    구조적 코드 둘러싸기

---
+++ 객체지향 사고 프로새스/6장. 객체를 사용해 설계하기/SomeMath.java
@@ -0,0 +1,9 @@
+
+public class SomeMath {
+	
+	public int add(int a, int b) {
+		
+		return a + b;
+	
+	}
+}
---
+++ 객체지향 사고 프로새스/6장. 객체를 사용해 설계하기/TestMath.java
@@ -0,0 +1,16 @@
+
+public class TestMath {
+
+	public static void main(String[] args) {
+
+		int x = 0;
+		
+		SomeMath math = new SomeMath();
+		
+		x = math.add(5, 8);
+		
+		System.out.println("x = " + x);
+		
+	}
+
+}
