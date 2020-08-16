2e55f60fec614059400f6916571546576cc09f33
me@yeonghoon.kim <me@yeonghoon.kim>
2020-08-16 15:54:39 +0900

    이식하기 어려운 코드를 둘러싸기

---
+++ 객체지향 사고 프로새스/6장. 객체를 사용해 설계하기/Sound.java
@@ -0,0 +1,8 @@
+
+public class Sound {
+
+	public void beep() {
+		System.out.println("\007");
+	}
+	
+}
---
+++ 객체지향 사고 프로새스/6장. 객체를 사용해 설계하기/TestBeep.java
@@ -0,0 +1,7 @@
+
+public class TestBeep {
+	public static void main(String[] args) {
+		Sound mySound = new Sound();
+		mySound.beep();
+	}
+}
