6c99ef3cab950c6d51f6691ab35e1e9f8364ae99
me@yeonghoon.kim <me@yeonghoon.kim>
2020-08-16 15:41:23 +0900

    구조적 코드

---
+++ 객체지향 사고 프로새스/6장. 객체를 사용해 설계하기/MainApplication.java
@@ -0,0 +1,15 @@
+
+public class MainApplication {
+	public static void main(String[] args) {
+		int x = 0;
+		
+		while(x <= 10) {
+			
+			if(x == 5) {
+				System.out.println("x = " + x);
+			}
+			
+			x++;
+		}
+	}
+}
