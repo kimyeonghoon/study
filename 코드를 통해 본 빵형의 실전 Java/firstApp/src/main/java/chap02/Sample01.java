ad49ff0aa8d7e2e7cefe92c3cb36784a68668643
kimyeonghoon <me@yeonghoon.kim>
2020-11-29 14:25:46 +0900

    기본자료형까지 봄. 68p

---
+++ 코드를 통해 본 빵형의 실전 Java/firstApp/src/main/java/chap02/Sample01.java
@@ -0,0 +1,10 @@
+package chap02;
+
+public class Sample01 {
+	public static void main(String[] args) {
+		char chr = 'A';
+		System.out.println("char : " + chr);
+		System.out.println("Encoding : " + (int)chr);
+		System.out.println("Decoding : " + (char)65);
+	}
+}
